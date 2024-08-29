2ad10b537f2ebd7b16a8b83b45b0cbb3ae77e735
shu <chukichi915@gmail.com>
2024-08-29 13:09:23 +0900

    ReactでTodoを作りました

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    { text: "todo1", isCompleted: false },
+    { text: "todo2", isCompleted: false },
+    { text: "todo3", isCompleted: false }
+  ]);
+
+  const [newTodoText, setNewTodoText] = useState('');
+
+  const [editText, setEditText] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const onChangeNewTodoText = (e) => {
+    setNewTodoText(e.target.value)
+  }
+
+  const onClickCreateNew = () => {
+    if (newTodoText === "") {
+      return
+    }
+    const newTodos = [...todos, { text: newTodoText, isCompleted: false }]
+    setTodos(newTodos);
+    setNewTodoText('');
+  }
+
+  const onClickDelete = (index) => {
+    const isConfirmed = window.confirm('本当に削除していいですか？');
+    if (isConfirmed) {
+      const newTodos = [...todos]
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    }
+  }
+  const onClickEdit = (index) => {
+    setEditIndex(index);
+    setEditText(todos[index].text);
+  }
+
+  const onChangeEditText = (e) => {
+    setEditText(e.target.value)
+  }
+
+
+  const onClickUpdate = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].text = editText;
+    setTodos(newTodos);
+    setEditIndex(null);
+  }
+
+  const onChangeCompleted = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="inner">
+
+        <h2>React Todo</h2>
+        <div className="create-wrap">
+          <input type="text" className="create-input" value={newTodoText} onChange={onChangeNewTodoText} />
+          <button className="create-button" onClick={onClickCreateNew}>登録</button>
+        </div>
+        <ul>
+
+          {todos.map((todo, index) => (
+            <li key={todo.text}>
+              {editIndex === index ? (
+                <>
+                  <input type="text" value={editText} onChange={onChangeEditText} />
+                  <button onClick={() => onClickUpdate(index)}>更新</button>
+                </>
+              ) : (
+                <>
+                  <input type="checkbox" checked={todo.isCompleted} onChange={() => onChangeCompleted(index)} />
+                  <span>{todo.text}</span>
+                  <button onClick={() => onClickEdit(index)}>編集</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
+                </>
+              )}
+            </li>
+          )
+          )}
+        </ul>
+        <div className="counter-wrap">
+          <p>未完了:{todos.filter(todo => todo.isCompleted === false).length}</p>
+          <p>完了:{todos.filter(todo => todo.isCompleted === true).length}</p>
+          <p>合計:{todos.length}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
